0f3652d8cc22f21c7ba2ac2832a2ca7b3ac5dad6
powe97 <116031952+powe97@users.noreply.github.com>
2024-03-06 16:16:36 -0600

    Lint

--- transfer_scraper/main.py
+++ transfer_scraper/main.py
@@ -232,7 +232,9 @@ def parse_course_td(td, note=None):
         cr_delim = (
             len(course_info)
             - 1
-            - list(bool(re.search(r"^\([0-9]", s.strip())) for s in course_info[::-1]).index(True)
+            - list(
+                bool(re.search(r"^\([0-9]", s.strip())) for s in course_info[::-1]
+            ).index(True)
         )
         assert bool(re.search(r"[0-9]\)", course_info[-1]))
     except (ValueError, AssertionError):
